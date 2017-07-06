48895d45f1c0a4c0c650293913f445f1706d106c
Aaron Plattner <aplattner@nvidia.com>
2017-07-06 09:36:22 -0700

    Use /usr/bin/env python3 consistently
    It's /usr/local/bin/python3 on FreeBSD

--- src/generate/eglFunctionList.py
+++ src/generate/eglFunctionList.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 """
 Contains a list of EGL functions to generate dispatch functions for.
--- src/generate/gen_egl_dispatch.py
+++ src/generate/gen_egl_dispatch.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 """
 Generates dispatch functions for EGL.
